72cd61fe9e0d7ca0c711c000a43c5fba21296c3c
Nokimochi1 <75022556+Nokimochi1@users.noreply.github.com>
2020-11-27 10:39:02 +0100

    Update inGameBot.py

--- inGameBot.py
+++ inGameBot.py
@@ -3,23 +3,20 @@ import numpy as np
 import os
 from time import time
 from windowCapture import WindowCapture
+from vision import findClickPositions
 
-
-wincap = WindowCapture("League of Legends (TM) Client")
+wincap  = WindowCapture("League of Legends (TM) Client")
 
 loop_time = time()
 while True:
-
+   
    screenshot = wincap.get_screenshot()
+   findClickPositions("minion.png", screenshot, 0.28, 'rectangles')
+   #cv.imshow("Liga", screenshot)
 
-   cv.imshow("tak to widze", screenshot)
-
-   print('FPS {}'.format(1 / (time() - loop_time)))
+   print("FPS {}".format(1 / (time() - loop_time)))
    loop_time = time()
 
    if cv.waitKey(1) == ord("q"):
       cv.destroyAllWindows() 
       break
-
-print("done")
-      
