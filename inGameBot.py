a56974006742d793e24dac92a1c4d06c1d79423e
Nokimochi1 <75022556+Nokimochi1@users.noreply.github.com>
2020-11-28 03:34:14 +0800

    Update inGameBot.py

--- inGameBot.py
+++ inGameBot.py
@@ -3,15 +3,17 @@ import numpy as np
 import os
 from time import time
 from windowCapture import WindowCapture
-from vision import findClickPositions
+from vision import Vision
 
 wincap  = WindowCapture("League of Legends (TM) Client")
+blueBuff = Vision("minion.png")
 
 loop_time = time()
 while True:
    
    screenshot = wincap.get_screenshot()
-   findClickPositions("minion.png", screenshot, 0.28, 'rectangles')
+   
+   points = blueBuff.find(screenshot, 0.35, "rectangles")
    #cv.imshow("Liga", screenshot)
 
    print("FPS {}".format(1 / (time() - loop_time)))
